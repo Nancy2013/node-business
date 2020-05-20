3de5520df40ac2594e3b1b3ebdddc8231757931b
Juliette.Wang <nannan.wang@broadlink.com.cn>
2020-05-20 16:38:59 +0800

    添加import支持；

---
+++ server/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-20 16:37:51
+ * @LastEditTime: 2020-05-20 16:38:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \node-business\server\index.js
+ */
+require('babel-core/register');
+require('./app.js');
