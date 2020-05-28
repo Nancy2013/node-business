590547307f3118831bd03574dc18ae449df85fe7
Juliette.Wang <nannan.wang@broadlink.com.cn>
2020-05-28 17:46:56 +0800

    捕获请求错误；添加数据返回处理；

--- server/common/utils.js
+++ server/common/utils.js
@@ -1,9 +1,25 @@
 /*
  * @Author: your name
  * @Date: 2020-05-28 15:09:03
- * @LastEditTime: 2020-05-28 15:15:11
+ * @LastEditTime: 2020-05-28 17:45:44
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \node-business\server\common\ultis.js
  */
 
+function sendDatas(data, errcode = 200, msg = 'success') {
+  let datas = { errcode, msg };
+  if (data) {
+    datas = {
+      ...datas,
+      data,
+    };
+   }
+  console.log(datas);
+
+  return datas;
+}
+
+module.exports = {
+  sendDatas,
+};
--- server/controller/account/account.js
+++ server/controller/account/account.js
@@ -1,43 +1,44 @@
 /*
  * @Author: your name
  * @Date: 2020-05-19 16:32:59
- * @LastEditTime: 2020-05-28 14:56:50
+ * @LastEditTime: 2020-05-28 17:44:20
  * @LastEditors: Please set LastEditors
  * @Description: In account Settings Edit
  * @FilePath: \node-business\server\controller\account\index.js
  */
 
 const accountModel = require('../../models/account');
+const { sendDatas } = require('../../common/utils');
+const errorCode = require('../../common/error');
 
-const { isEmpty } = global.$lodash;
+const {
+  isEmpty
+} = global.$lodash;
 
 
 const accountController = {
   login: async (req, res) => {
+    let response;
     const { accountname, password } = req.body;
-    const result = await accountModel.find({ accountname });
-    console.log(result);
-
-    if (isEmpty(result)) {
-      res.send({
-        errcode: 404,
-        msg: '用户不存在'
-      });
-     }
-    if (result.password !== password) {
-      res.send({
-        errcode: 500,
-        msg: '密码错误'
-      });
-    }
-    const { uid, token, pid } = result;
-    const user = {
-      uid,
-      token,
-      pid,
-      accountinfo: result,
-    };
-    res.send(user);
+    accountModel.find({ accountname }).then(result => {
+      if (isEmpty(result)) {
+       response = sendDatas(null, errorCode.forbidden, '用户不存在');
+      } else if (result[0].accountpwd !== password) {
+        response = sendDatas(null, errorCode.forbidden, '密码错误');
+      } else {
+        const { uid, token, pid } = result[0];
+        const data = {
+          uid,
+          token,
+          pid,
+          accountinfo: result,
+        };
+        response = sendDatas(data);
+      }
+      res.send(response);
+    }).catch(e => {
+      console.error(e);
+    });
   },
   logout: async (req, res) => {
     console.log('logout');
@@ -52,7 +53,9 @@ const accountController = {
     res.send(result);
   },
   mod: async (req, res) => {
-    const { _id } = req.params;
+    const {
+      _id
+    } = req.params;
     const params = req.body;
     const result = await accountModel.findByIdAndUpdate(_id, params);
     res.send({
@@ -62,6 +65,6 @@ const accountController = {
   },
   del: async (req, res) => {
     console.log('del');
-   },
+  },
 };
 module.exports = accountController;
