0c128040bd7a718860a0d5c963585d97fde26f61
Juliette.Wang <nannan.wang@broadlink.com.cn>
2020-06-01 14:13:54 +0800

    修改routes引入；

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,21 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 /*
  * @Author: your name
  * @Date: 2020-05-18 15:37:39
- * @LastEditTime: 2020-05-27 15:11:27
+ * @LastEditTime: 2020-06-01 14:13:28
  * @LastEditors: Please set LastEditors
  * @Description: In account Settings Edit
  * @FilePath: \node-business\server\views\index.js
  */
+const fs = require('fs');
 const global = require('./../common/global');
-const account = require('./account');
-const project = require('./project');
+
+const files = fs.readdirSync(`${__dirname}`).filter(v => v !== 'index.js').map(v => v.slice(0, v.length - 3));
+const modules = files.map(v => require(`./${v}`));
 
 module.exports = (app) => {
-  app.use(`${global.URL_DEFAULT_PREFIXER}/account`, account);
-  app.use(`${global.URL_DEFAULT_PREFIXER}/project`, project);
+  modules.forEach((v, index) => {
+    app.use(`${global.URL_DEFAULT_PREFIXER}/${files[index]}`, v);
+  });
 };
