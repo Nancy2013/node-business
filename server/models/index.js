76bdc628681144bcaafb58d15d2c8e1c0857282a
Juliette.Wang <nannan.wang@broadlink.com.cn>
2020-06-03 09:30:14 +0800

    install inflection;

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-18 15:37:39
- * @LastEditTime: 2020-06-02 11:37:13
+ * @LastEditTime: 2020-06-03 09:29:54
  * @LastEditors: Please set LastEditors
  * @Description: In account Settings Edit
  * @FilePath: \node-business\server\views\index.js
