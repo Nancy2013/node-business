e8ff802534f94f17bb4937b6a8f9e03f2da47b73
Juliette.Wang <nannan.wang@broadlink.com.cn>
2020-10-29 16:28:11 +0800

    添加发布文件；

---
+++ dist/static/config.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-15 17:41:56
+ * @LastEditTime: 2020-05-27 15:18:21
+ * @LastEditors: Please set LastEditors
+ */
+const PROJECT = {
+  id: 2,
+  projectname: '行业服务平台',
+  loginuiurl: '',
+};
